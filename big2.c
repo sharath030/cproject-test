6e36d1f4dacbd9a6a40fc2c323daa47d490d8d36
sharath030 <96613553+sharath030@users.noreply.github.com>
2022-01-01 14:05:10 +0530

    Update big2.c

--- big2.c
+++ big2.c
@@ -3,7 +3,7 @@ big2()
 {
     int num1, num2;
     // Ask user to enter the two numbers
-    printf("Please Enter Two different values\n");
+    printf("\nPlease Enter Two different values\n");
     // Read two numbers from the user
     scanf("%d %d", &num1, &num2);
     if(num1 > num2)
